0051480bb184128a39f99dfc2631041e034ab4cd
avneetvirdi <avneetvirdi26@gmail.com>
2024-03-14 20:00:10 -0400

    initial-commit

---
+++ src/com/patterns/Pattern3.java
@@ -0,0 +1,20 @@
+package com.patterns;
+import java.util.Scanner;
+
+public class Pattern3 {
+    // 1
+    // 12
+    // 123
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "\t");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ src/com/patterns/Pattern4.java
@@ -0,0 +1,20 @@
+package com.patterns;
+import java.util.Scanner;
+
+public class Pattern4 {
+    // 1
+    // 22
+    // 333
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "\t");
+            }
+            System.out.println("");
+        }
+    }
+}
